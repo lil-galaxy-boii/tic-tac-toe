154b20164487d556a9bfecb93ae36d386e0da263
Nate Schenck <lil-galaxy-boii@users.noreply.github.com>
2016-11-10 13:58:51 -0800

    Made Black Squares + Red Circle One

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,13 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+function topLeftShape() {
+var canvas= document.getElementById  ("game-board")
+if (topLeftShape == "circle") {
+var circle = document.createElementNS (namespace,"circle")
+circle.setAttribute ("cx",45)
+circle.setAttribute ("cy",45)
+circle.setAttribute ("r",30)
+circle.setAttribute ("fill","red")
+canvas.appendChild(circle)
+}
