87e62eee9b525bbfdf82867bb1a90c9f492ef8b1
Nate Schenck <lil-galaxy-boii@users.noreply.github.com>
2016-11-15 14:02:04 -0800

    Fixed issues, added another shape?

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,25 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var currentShape = "circle"
 function topLeftShape() {
 var canvas= document.getElementById  ("game-board")
-if (topLeftShape == "circle") {
+if (currentShape == "circle") {
 var circle = document.createElementNS (namespace,"circle")
 circle.setAttribute ("cx",45)
 circle.setAttribute ("cy",45)
 circle.setAttribute ("r",30)
 circle.setAttribute ("fill","red")
 canvas.appendChild(circle)
+currentShape = "skinnyRect"
+} else {
+ var skinnyRect = document.createElementNS (namespace, "rect")
+  skinnyRect.setAttribute ("x",100)
+  skinnyRect.setAttribute ("y",50)
+  skinnyRect.setAttribute ("width",30)
+  skinnyRect.setAttribute ("length",20)
+  skinnyRect.setAttribute ("fill","orange")
+  canvas.appendChild(skinnyRect)
+  currentShape = "circle"
+  }
 }
