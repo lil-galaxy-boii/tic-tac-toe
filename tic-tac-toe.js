8a17f16449fab2db290517a64fa572c85136696d
Nate Schenck <lil-galaxy-boii@users.noreply.github.com>
2016-11-17 14:03:22 -0800

    f

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,21 +1,19 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-var clickedCircle1 = 0
+var turn = player1
 var currentShape = "circle"
 function leftLeftShape() {
 var canvas= document.getElementById  ("game-board")
-if (currentShape == "circle") {
+if (turn == "player1") {
 var circle = document.createElementNS (namespace,"circle")
 circle.setAttribute ("cx",45)
 circle.setAttribute ("cy",47)
 circle.setAttribute ("r",20)
 circle.setAttribute ("fill","red")
 canvas.appendChild(circle)
-clickedCircle1 = 1
-currentShape = "skinnyRect"
+turn = "player2"
 } else {
-  var clickedRect1 = 0
  var skinnyRect = document.createElementNS (namespace, "rect")
   skinnyRect.setAttribute ("x",23)
   skinnyRect.setAttribute ("y",25)
@@ -23,25 +21,22 @@ currentShape = "skinnyRect"
   skinnyRect.setAttribute ("height",45)
   skinnyRect.setAttribute ("fill","orange")
   canvas.appendChild(skinnyRect)
-  currentShape = "circle"
-  clickedRect1 = 1
+  turn = "player1"
   }
 }
-var clickedCircle2 = 0
+var turn = "player1"
 var currentShape = "smallmoon"
 function midLeftShape() {
 var canvas= document.getElementById  ("game-board")
-if (currentShape == "smallmoon") {
+if (turn == "player1") {
 var smallmoon = document.createElementNS (namespace,"circle")
 smallmoon.setAttribute ("cx",145)
 smallmoon.setAttribute ("cy",47)
 smallmoon.setAttribute ("r",20)
 smallmoon.setAttribute ("fill","red")
 canvas.appendChild(smallmoon)
-clickedCircle2 = 1
-currentShape = "orangebox"
+turn = "player2"
 } else {
-  clickedRect2 = 0
  var orangebox = document.createElementNS (namespace, "rect")
   orangebox.setAttribute ("x",123)
   orangebox.setAttribute ("y",25)
@@ -49,25 +44,22 @@ currentShape = "orangebox"
   orangebox.setAttribute ("height",45)
   orangebox.setAttribute ("fill","orange")
   canvas.appendChild(orangebox)
-  currentShape = "smallmoon"
-  clickedRect2 = 1
+  turn = "player1"
   }
 }
-var clickedCircle3 = 0
+var turn = "player1"
 var currentShape = "smallCherry"
 function rightLeftShape() {
 var canvas= document.getElementById  ("game-board")
-if (currentShape == "smallCherry") {
+if (turn == "player1") {
 var smallCherry = document.createElementNS (namespace,"circle")
 smallCherry.setAttribute ("cx",245)
 smallCherry.setAttribute ("cy",47)
 smallCherry.setAttribute ("r",20)
 smallCherry.setAttribute ("fill","red")
 canvas.appendChild(smallCherry)
-clickedCircle3 = 1
-currentShape = "boxFruit"
+turn= "player2"
 } else {
-  clickedRect3 = 0
  var boxFruit = document.createElementNS (namespace, "rect")
   boxFruit.setAttribute ("x",223)
   boxFruit.setAttribute ("y",25)
@@ -75,7 +67,6 @@ currentShape = "boxFruit"
   boxFruit.setAttribute ("height",45)
   boxFruit.setAttribute ("fill","orange")
   canvas.appendChild(boxFruit)
-  currentShape = "smallCherry"
-  clickedRect3 = 1
+turn = "player1"
   }
 }
